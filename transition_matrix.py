fcf5305b2a1199a42bb00e5ce4058779d4b18cf5
byung2685 <byung2685@gmail.com>
2020-01-24 12:51:58 +0900

    transition matrix

--- transition_matrix.py
+++ transition_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.io import mmread
 import scipy.sparse
 
 def raw_check(transition_matrix_raw) :
@@ -26,27 +27,46 @@ def preprocessing(text_raw_path) :
         col_index.append(int(elem_parse[1])-1)
         values.append(float(elem_parse[2]))
 
+    # If I should make the matrix "symmetric"?
+    # row_copy = row_index.copy()
+    # row_index.extend(col_index)
+    # col_index.extend(row_copy)
+    # values.extend(values)
+    # num_docs = max(row_index) + 1
     num_row = max(row_index)
     num_col = max(col_index)
     num_docs = max(num_row, num_col) + 1
     print("# of Documents : {}".format(num_docs))
-    transition_matrix = scipy.sparse.coo_matrix((values, (row_index, col_index)), shape=(num_docs,num_docs))
+    transition_matrix = scipy.sparse.coo_matrix((values, (row_index, col_index)), shape=(num_docs,num_docs)).tocsr()
+    # print("hello")
+    # with open(text_raw_path, "r") as f :
+    #     transition_matrix = mmread(text_raw_path)
+    # transition_matrix = mmread(text_raw_path)
+    # print(transition_matrix.shape)
+    # exit()
     #### Preprocessing ####
     # matrix.sum(axis=0) : sum all rows, shape: (row,1)
     # matrix.sum(axis=1) : sum all columns, shape: (1,col)
-    sum_elem = transition_matrix.sum(axis=1)
-    zero_idx = sum_elem==0
-    sum_elem[zero_idx] = -1
-    nonzero_idx = sum_elem>0
-    sum_elem[nonzero_idx] = 0
-    # sum_elem = scipy.sparse.csr_matrix(np.tile(sum_elem, num_docs))
-    sum_elem = scipy.sparse.hstack([scipy.sparse.csr_matrix(sum_elem) for _ in range(num_docs)])
-    print("Sum finished")
-    transition_matrix -= sum_elem
-    print("Filled")
+    # sum_elem = transition_matrix.sum(axis=1)
+    # nonzero_row = sum_elem.nonzero()[0]
+    # zero_idx = sum_elem==0
+    # sum_elem[zero_idx] = -1
+    # nonzero_idx = sum_elem>0
+    # sum_elem[nonzero_idx] = 0
+    # sum_elem = scipy.sparse.hstack([scipy.sparse.csr_matrix(sum_elem) for _ in range(num_docs)])
+    # print("# of non-zero rows : ", len(nonzero_row))  # of nonzero column : 76586 (non-symmetric ver.)
+    # for i in range(num_docs) :
+    #     if i not in nonzero_row :
+    #         transition_matrix[i] = 1
     norm_factor = transition_matrix.sum(axis=1)
+    nonzero_row = norm_factor.nonzero()[0]
+    print("# of non-zero rows : ", len(nonzero_row))
     transition_matrix = transition_matrix.multiply(1/norm_factor)
-    print(type(transition_matrix))
+    print("Normalized")
+    transition_matrix = transition_matrix.tolil()
+    for i in range(num_docs) :
+        if i not in nonzero_row :
+            transition_matrix[i] = 1/num_docs
     scipy.sparse.save_npz("./data/transition_matrix.npz", transition_matrix)
     # transition_matrix = scipy.sparse.load_npz("./data/transition_matrix.npz")
     print("Successfully Saved to './data/transition_matrix.npz'!")
