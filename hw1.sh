f50e57b4e002d9dffa0d74edda9de8e9f452bf80
byung2685 <byung2685@gmail.com>
2020-02-07 04:32:07 +0900

    Submission ver.

--- hw1.sh
+++ hw1.sh
@@ -2,8 +2,8 @@
 
 conda create -n mlhw1_byungjoo python=3.6.7
 conda actiavte mlhw1_byungjoo
-pip install numpy=1.18.1
-pip install scipy=1.4.1
+pip install numpy==1.18.1
+pip install scipy==1.4.1
 pip install overrides
 python main.py --pagerank gpr --criterion ns
 python main.py --pagerank gpr --criterion ws
